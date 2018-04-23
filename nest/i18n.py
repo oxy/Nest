ec2e7e07a9302d232eb80aa41eaf3ff54d1c2dfb
Oxylibrium <oxylibrium@gmail.com>
2018-04-23 09:00:24 +0530

    feat(modules/i18n): add language names
    - Add `I18n.load_locales()` and corresponding placeholder data
    - Add `I18n.locales()` to list locales with names
    - Add `I18n.lang` to return current language
    - Make docstrings more consistent

--- nest/i18n.py
+++ nest/i18n.py
@@ -4,6 +4,7 @@ Implement internationalization on a per-module level.
 import json
 import logging
 import os
+from typing import Dict
 
 from nest.helpers import dictwalk
 
@@ -13,12 +14,53 @@ class I18n:
     Attributes
     ----------
     locale: str
-        Default locale to use if a string is not present in the given locale.
+        Default locale.
     '''
     def __init__(self, locale: str):
         self._i18n_data = {}
         self._logger = logging.getLogger('nest.i18n')
         self.locale = locale
+        self.load_locales()
+
+    def load_locales(self):
+        '''Load core data about each supported locale.'''
+        directory = os.path.dirname(os.path.realpath(__file__))
+        path = os.path.join(directory, 'i18n.json')
+
+        with open(path) as datafile:
+            data = json.load(datafile)
+
+        for lang, lang_data in data.items():
+            if lang not in self._i18n_data:
+                self._logger.debug(f'Registering lang {lang}')
+                self._i18n_data[lang] = {}
+
+            self._i18n_data[lang].update(lang_data)
+
+    def locales(self, current_locale: str) -> Dict[str, str]:
+        '''Return dictionary of language data.
+
+        Parameters
+        ----------
+        current_locale: str
+            Locale to use (truncated to first two characters)
+        '''
+
+        locales = {}
+        current_lang = current_locale[:2]
+        for locale, data in self._i18n_data.items():
+            lang = locale[:2]
+            try:
+                l_user = data['names'].get(
+                    current_lang,
+                    data['names'].get(self.lang))
+
+                l_native = data['names'].get(lang)
+                locales[locale] = {'user': l_user, 'native': l_native}
+            except (KeyError, ValueError):
+                self._logger.warning(f'{locale} has no name data! Ignoring.')
+                continue
+        return locales
 
     def load_module(self, module):
         '''Load language data for a module.
@@ -68,3 +110,8 @@ class I18n:
             except ValueError:
                 item = string
         return item
+
+    @property
+    def lang(self):
+        '''Default language.'''
+        return self.locale[:2]
