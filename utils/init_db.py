6ac1a13aa7aab30ed3801a8261134026285983fb
Oxylibrium <oxylibrium@gmail.com>
2018-04-28 18:36:06 +0530

    fix(utils/init_db): move queries into constant

--- utils/init_db.py
+++ utils/init_db.py
@@ -5,20 +5,25 @@ Module to initialize the database with the required tables.
 import asyncio
 import asyncpg
 
+QUERIES = ['''
+    CREATE TABLE prefix(
+        id numeric(21, 0) PRIMARY KEY,
+        user_prefix text,
+        mod_prefix text
+    )
+    ''',
+    '''
+    CREATE TABLE locale(
+        id numeric(21, 0) PRIMARY KEY,
+        locale text
+    );
+    ''']
+
 async def initialize(database):
     conn = await asyncpg.connect(database=database)
     # TODO: Read configuration data from modules.
-    await conn.executemany('''
-        CREATE TABLE prefix(
-            id numeric(21, 0) PRIMARY KEY,
-            user_prefix text,
-            mod_prefix text
-        );
-        CREATE TABLE locale(
-            id numeric(21, 0) PRIMARY KEY,
-            locale text
-        );
-    ''', None)
+    for query in QUERIES:
+        await conn.execute(query)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
