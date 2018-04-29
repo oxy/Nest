92c76d77322e9ae80f7cf2a72a697c98ed409ebe
Oxylibrium <oxylibrium@gmail.com>
2018-04-29 15:49:42 +0530

    feat(modules/core): add ping command

---
+++ modules/core/__init__.py
@@ -0,0 +1,18 @@
+'''
+Provides core features, such as ping.
+'''
+import time
+from discord.ext import commands
+
+class CoreCommands:
+    category = 'user'
+
+    @commands.command()
+    async def ping(self, ctx):
+        pre_typing = time.monotonic()
+        await ctx.trigger_typing()
+        latency = int(round((time.monotonic() - pre_typing) * 1000))
+        await ctx.send(ctx._('ping_response').format(latency))
+
+def setup(bot):
+    bot.add_cog(CoreCommands())
