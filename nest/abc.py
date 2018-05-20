8543201a6a6f3dbec198f0d533a4178a400d30a9
Oxylibrium <oxylibrium@gmail.com>
2018-05-20 05:39:38 +0530

    docs(abc): add docs to nest.abc

--- nest/abc.py
+++ nest/abc.py
@@ -1,6 +1,7 @@
 """Provides abstract base classes for certain functionality."""
 
 import abc
+from discord.ext import commands
 
 class Provider(abc.ABC):
     """
@@ -14,14 +15,35 @@ class Provider(abc.ABC):
     Methods
     -------
     get:
-        Fetches data 
+        Fetches data
     """
     provides: str
 
     @abc.abstractmethod
-    async def get(self, ctx):
+    async def get(self, ctx: commands.Context):
+        """
+        Gets data for a specific context.
+
+        Parameters
+        ----------
+        ctx: commands.Context
+            Context to get data for.
+        Returns
+        -------
+        Data for the given context.
+        """
         raise NotImplementedError
-    
+
     @abc.abstractmethod
-    async def set(self, ctx, data):
+    async def set(self, ctx: commands.Context, data):
+        """
+        Sets data for a specific context.
+
+        Parameters
+        ----------
+        ctx: commands.Context
+            Context to set data for.
+        data:
+            Data to set.
+        """
         raise NotImplementedError
