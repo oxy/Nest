696e6d1354d8047c223546c2c37920a04ab06cb0
Oxylibrium <oxylibrium@gmail.com>
2018-10-26 17:55:35 +0530

    feat(modules/utils): add npm to PackageLookups

--- modules/utils/dev.py
+++ modules/utils/dev.py
@@ -11,6 +11,9 @@ URL_PYPI_API = "https://pypi.python.org/pypi/{package}/json"
 URL_PYPI_PACKAGE = "https://pypi.python.org/pypi/{package}"
 FIELDS_PYPI = {"license", "docs_url", "home_page", "requires_python", "author"}
 
+URL_NPM_API = "https://registry.npmjs.org/{package}/{version}"
+FIELDS_NPM = {"license", "homepage"}
+
 
 class PackageLookups:
     category = "user"
@@ -47,4 +50,40 @@ class PackageLookups:
                 embed.add_field(name=ctx._(field), value=info[field])
 
         embed.set_thumbnail(url="http://i.imgur.com/1Pp5s56.png")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def npm(self, ctx, package: str, version: str = "latest"):
+        """
+        Look up a package on the official Node.js package manager registry.
+        """
+        data_url = URL_NPM_API.format(package=package, version=version)
+
+        async with ctx.bot.session.get(data_url) as resp:
+            if not resp.status in [200, 404]:
+                raise exceptions.WebAPIInvalidResponse(
+                    api="NPMjs", status=resp.status
+                )
+
+            if resp.status == 404:
+                await ctx.send("{package} isn't a package on PyPI!")
+                return
+
+            info = await resp.json()
+
+        embed = discord.Embed(
+            title=f"{info['name']} `({info['version']})`",
+            description=helpers.smart_truncate(info["description"]),
+            url=URL_PYPI_PACKAGE.format(package=package),
+        )
+
+        if "author" in info:
+            embed.add_field(name=ctx._("author"), value=info["author"]["name"])
+
+        for field in FIELDS_NPM & info.keys():
+            embed.add_field(name=ctx._(field), value=info[field])
+
+        embed.set_thumbnail(
+            url="https://raw.githubusercontent.com/npm/logos/master/npm%20square/n-64.png"
+        )
         await ctx.send(embed=embed)
