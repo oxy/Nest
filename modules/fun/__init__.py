9d9b21f3d999c733a9fe9c92d3f8414e07864267
Oxylibrium <oxylibrium@gmail.com>
2018-10-28 17:18:12 +0530

    feat(modules/fun): add xkcd command

--- modules/fun/__init__.py
+++ modules/fun/__init__.py
@@ -3,6 +3,8 @@ Provides various classes of fun commands.
 """
 
 from .dev import DeveloperFun
+from .comics import Comics
 
 def setup(bot):
     bot.add_cog(DeveloperFun())
+    bot.add_cog(Comics())
---
+++ modules/fun/comics.py
@@ -0,0 +1,50 @@
+"""
+Commands to search for comics.
+"""
+
+import discord
+from discord.ext import commands
+
+from nest import exceptions
+
+
+class Comics:
+    category = "user"
+
+    @commands.command()
+    async def xkcd(self, ctx, number: int = None):
+        """
+        Grabs a (or the latest) comic from xkcd and shows it.
+        """
+
+        if number:
+            url = f"https://xkcd.com/{number}/info.0.json"
+        else:
+            url = f"https://xkcd.com/info.0.json"
+
+        async with ctx.bot.session.get(url) as resp:
+            if resp.status not in [200, 404]:
+                raise exceptions.WebAPIInvalidResponse(
+                    api="xkcd", status=resp.status
+                )
+
+            if resp.status == 404:
+                await ctx.send("{number} isn't a comic on XKCD!")
+                return
+
+            data = await resp.json()
+
+        number = data["num"]
+        image = data["img"]
+        title = data["safe_title"]
+        day = data["day"]
+        month = data["month"]
+        year = data["year"]
+
+        link = f"https://xkcd.com/{number}"
+
+        embed = discord.Embed(title=f"{number} - **{title}**", url=link)
+        embed.set_footer(text=ctx._("published") + f"{year}{month}{day}")
+        embed.set_image(url=image)
+
+        await ctx.send(embed=embed)
