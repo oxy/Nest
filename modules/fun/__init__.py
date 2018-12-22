d7f0454b4826d4301d616ca32bbfe36cc13793a9
Oxylibrium <oxylibrium@gmail.com>
2018-12-22 18:17:08 +0530

    feat(modules/fun): add some RNG based commands

--- modules/fun/__init__.py
+++ modules/fun/__init__.py
@@ -4,7 +4,11 @@ Provides various classes of fun commands.
 
 from .dev import DeveloperFun
 from .comics import Comics
+from .random import RandomCommands
+
 
 def setup(bot):
     bot.add_cog(DeveloperFun())
     bot.add_cog(Comics())
+    bot.add_cog(RandomCommands())
+
---
+++ modules/fun/random.py
@@ -0,0 +1,39 @@
+"""
+RNG commands.
+"""
+
+import random
+from discord.ext import commands
+
+
+class RandomCommands:    
+    category = "user"
+
+    @commands.command(aliases=["coinflip"])
+    async def coin(self, ctx):
+        """
+        Flips a coin randomly and gives you the result.
+        """
+
+        choice = random.choice(["heads", "tails"])
+        await ctx.send(ctx._(choice))
+
+    @commands.command(aliases=["diceroll"])
+    async def dice(self, ctx, diceroll: str = '1d6'):
+        """
+        Rolls a die wiithh input in the AdX notation.
+        """
+        times, num = diceroll.split('d')
+        times = int(times) if times else 1
+        num = int(num) if num else 6
+        maxscore = times*num
+        score = random.randint(times, maxscore)
+        await ctx.send(ctx._("roll_result").format(score=score, maxscore=maxscore))
+        
+
+    @commands.command()
+    async def rate(self, ctx, *args):
+        content = ' '.join(args)
+        num = random.randint(0, 10)
+        await ctx.send(ctx._("rating").format(content=content, num=num))
+
