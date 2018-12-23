5afe005bb138de34dc8ac0a1b234b7ac937e22ff
Oxylibrium <oxylibrium@gmail.com>
2018-12-23 18:05:08 +0530

    feat(modules/lookups): add osu account lookup command

--- modules/lookups/gaming.py
+++ modules/lookups/gaming.py
@@ -6,11 +6,13 @@ from io import BytesIO
 
 import discord
 from discord.ext import commands
+from pycountry import countries
 
 from nest import exceptions
 
 URL_MCUUID_API = "https://api.mojang.com/users/profiles/minecraft/{user}"
 URL_MCSKIN_API = "https://visage.surgeplay.com/{image}/{uuid}.png"
+URL_OSU_API = "https://osu.ppy.sh/api/get_user"
 
 class GamingLookups:
     category = "user"
@@ -40,4 +42,45 @@ class GamingLookups:
 
         await ctx.send(file=discord.File(fp=image, filename=f"{uuid}.png"))
 
+    @commands.command()
+    async def osu(self, ctx, user: str):
+        query = {"k": ctx.bot.tokens["osu"], "u": user}
+
+        async with ctx.bot.session.get(URL_OSU_API, params=query) as response:
+            if response.status == 200:
+                data = await response.json()
+            else:
+                return
+
+        user = data[0]
+
+        keys = {
+            ctx._('Play Count'): 'playcount',
+            ctx._('Country'): 'country',
+            ctx._('Level'): 'level',
+            ctx._('Ranked Score'): 'ranked_score',
+            ctx._('Total Score'): 'total_score',
+            ctx._('Accuracy'): 'accuracy',
+            ctx._('{level} Ranking'.format(level='SS')): 'count_rank_ss',
+            ctx._('{level} Ranking'.format(level='S')): 'count_rank_s',
+            ctx._('{level} Ranking'.format(level='A')): 'count_rank_a',
+            '300s': 'count300',
+            '100s': 'count100',
+            '50s': 'count50'
+        }
+
+        userid = user['user_id']
+        username = user['username']
+
+        embed = discord.Embed(title=username, description=userid, url=f"https://osu.ppy.sh/u/{userid}")
+
+        # Convert some data in the dict before iterating through it
+        user['accuracy'] = round(float(user['accuracy']), 2)
+        user['country'] = countries.get(alpha_2='PH').name
+
+        for field, value in keys.items():
+            embed.add_field(name=field, value=user[value], inline=True)
+
+        embed.set_thumbnail(url=f"http://a.ppy.sh/{userid}")
+        await ctx.send(embed=embed)
 
