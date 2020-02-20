b6698eddc0b56a0c817640176c0c7db7af8c8c90
Oxylibrium <oxylibrium@gmail.com>
2020-02-20 13:26:00 +0530

    refactor(modules.db.db): add type hint for pool

--- modules/db/db.py
+++ modules/db/db.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 class PostgreSQL(commands.Cog):
     def __init__(self, bot):
         self._db = bot.options["database"]
+        self.pool: asyncpg.pool.Pool = None
 
     @commands.Cog.listener()
     async def on_ready(self):
