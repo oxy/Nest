cc0cf1d69051c41fa67646fdf2e2b061e210775f
Oxylibrium <oxylibrium@gmail.com>
2020-02-20 13:38:39 +0530

    fix(modules.core.prefix): fix prefix command

--- modules/core/prefix.py
+++ modules/core/prefix.py
@@ -1,17 +1,13 @@
 from discord.ext import commands
 
+
 class PrefixManager(commands.Cog):
     @commands.command(aliases=["whatprefix"])
     async def prefix(self, ctx, *_):
         """
         Get the bot's current prefixes.
         """
-        message = "```yml\n"
-        for key, prefix in ctx.prefixes:
-            key = ctx._(f"prefix_{key}")
-            message += f"{key}: {prefix}"
-        message += "```"
-        await ctx.send(message)
+        await ctx.send(ctx._("current_prefix").format(prefix=ctx.prefix))
 
     @commands.command()
     @commands.has_permissions(manage_server=True)
@@ -22,6 +18,4 @@ class PrefixManager(commands.Cog):
         """
 
         await ctx.bot.get_cog("PrefixStore").set(ctx, prefix)
-        await ctx.send(
-            ctx._("prefix_set_success").format(prefix=prefix)
-        )
+        await ctx.send(ctx._("prefix_set_success").format(prefix=prefix))
