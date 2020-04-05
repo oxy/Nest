a3d98c4872201c4ef2eb4be0fa820fe3ef7b4a42
Oxylibrium <oxylibrium@gmail.com>
2020-04-05 10:57:06 +0530

    fix(modules/db): initialize db before ready
    discord.py processes commands before the global on_ready is fired,
    so initialize DB connection on module load instead of on_ready

--- modules/db/db.py
+++ modules/db/db.py
@@ -4,8 +4,6 @@ from discord.ext import commands
 class PostgreSQL(commands.Cog):
     def __init__(self, bot):
         self._db = bot.options["database"]
-        self.pool: asyncpg.pool.Pool = None
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.pool = await asyncpg.create_pool(database=self._db)
+        self.pool = bot.loop.run_until_complete(
+            asyncpg.create_pool(database=self._db)
+        )
