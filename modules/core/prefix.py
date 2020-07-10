f44dacf2882ca2e0f7806739652e37320cde28aa
Oxylibrium <oxylibrium@gmail.com>
2020-07-10 11:13:42 +0530

    fix(modules.core.prefix): show correct prefix
    Fetch prefix from the DB helper instead of using `ctx.prefix`,
    which breaks when a mention is used.

--- modules/core/prefix.py
+++ modules/core/prefix.py
@@ -4,18 +4,16 @@ from discord.ext import commands
 class PrefixManager(commands.Cog):
     @commands.command(aliases=["whatprefix"])
     async def prefix(self, ctx, *_):
-        """
-        Get the bot's current prefixes.
-        """
-        await ctx.send(ctx._("current_prefix").format(prefix=ctx.prefix))
+        """Get the current prefix."""
+
+        prefix = await ctx.bot.get_cog("PrefixStore").get(ctx.message)
+        await ctx.send(ctx._("current_prefix").format(prefix=prefix))
 
     @commands.command()
     @commands.has_permissions(manage_guild=True)
     @commands.guild_only()
     async def setprefix(self, ctx, prefix: str):
-        """
-        Set a prefix for a guild.
-        """
+        """Set a prefix for a guild."""
 
         await ctx.bot.get_cog("PrefixStore").set(ctx, prefix)
         await ctx.send(ctx._("prefix_set_success").format(prefix=prefix))
